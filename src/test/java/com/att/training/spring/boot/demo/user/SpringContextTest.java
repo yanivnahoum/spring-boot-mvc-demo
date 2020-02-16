f7b46c0dabe06a94578e08b0cb06e93008b70868
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2020-02-16 15:25:24 +0200

    [build] Canceled data jpa auto configuration

--- src/test/java/com/att/training/spring/boot/demo/user/SpringContextTest.java
+++ src/test/java/com/att/training/spring/boot/demo/user/SpringContextTest.java
@@ -1,14 +1,29 @@
 package com.att.training.spring.boot.demo.user;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@Disabled("since the complete context requires an available mysql instance")
+// The complete context requires an available mysql instance"
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 class SpringContextTest {
+
+    @MockBean private UserRepository userRepository;
 
     @Test
     void contextLoads() {
     }
 }
+
+
