1bf2d0d7c0a3773cf036676605c02e0508fd95da
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2020-03-17 17:31:31 +0200

    [deps] Upgraded spring boot to 2.2.5, wrapper and various dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.2.4.RELEASE"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("org.springframework.boot") version "2.2.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("io.freefair.lombok") version "4.1.6"
 }
 
@@ -19,20 +19,24 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("com.google.guava:guava:28.2-jre")
-    implementation("org.jetbrains:annotations:17.0.0")
+    implementation("org.jetbrains:annotations:19.0.0")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     runtimeOnly("mysql:mysql-connector-java")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    val testContainersVersion = "1.12.5"
+    val testContainersVersion = "1.13.0"
     testImplementation("org.testcontainers:mysql:$testContainersVersion")
     testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
 }
 
 tasks.test {
     useJUnitPlatform {
-        if (!project.hasProperty("withITs")) excludeTags("slow")
+        if (project.hasProperty("onlyITs")) {
+            includeTags("slow")
+        } else if (!project.hasProperty("withITs")) {
+            excludeTags("slow")
+        }
     }
     testLogging {
         events("passed", "skipped", "failed")
