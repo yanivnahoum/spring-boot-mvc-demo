cd3d6da09db6d2bbedf8b53968d050cd11d28203
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2020-12-13 20:46:31 +0200

    [tests] Used @Nested test in AndStillAnotherUserControllerTest

--- src/test/java/com/att/training/spring/boot/demo/user/AndStillAnotherUserControllerTest.java
+++ src/test/java/com/att/training/spring/boot/demo/user/AndStillAnotherUserControllerTest.java
@@ -2,6 +2,7 @@ package com.att.training.spring.boot.demo.user;
 
 import com.att.training.spring.boot.demo.tc.SpringBootMySqlSingletonTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -39,32 +40,28 @@ class AndStillAnotherUserControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-}
 
-@SpringBootMySqlSingletonTest
-@AutoConfigureMockMvc
-@DisplayName("When calling GET /users/{id}")
-class GetSingleUser {
+    @Nested
+    @DisplayName("When calling GET /users/{id}")
+    class GetSingleUser {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Test
+        void givenId1_shouldReturn200OK_withMichaelAsJson() throws Exception {
+            mockMvc.perform(get("/users/{id}", 1))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("{'id':1,'firstName':'Michael','lastName':'Jordan','age':50}"))
+                    .andExpect(jsonPath("$.firstName", is("Michael")));
+        }
 
-    @Test
-    void givenId1_shouldReturn200OK_withMichaelAsJson() throws Exception {
-        mockMvc.perform(get("/users/{id}", 1))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().json("{'id':1,'firstName':'Michael','lastName':'Jordan','age':50}"))
-                .andExpect(jsonPath("$.firstName", is("Michael")));
-    }
-
-    @Test
-    void givenMissingId_shouldReturn404NotFound() throws Exception {
-        int id = 4;
-        String expectedJson = String.format("{ code: 5001, message = 'UserDto not found: %d' }", id);
-        mockMvc.perform(get("/users/{id}", id))
-                .andDo(print())
-                .andExpect(status().isNotFound())
-                .andExpect(content().json(expectedJson));
+        @Test
+        void givenMissingId_shouldReturn404NotFound() throws Exception {
+            int id = 4;
+            String expectedJson = String.format("{ code: 5001, message = 'UserDto not found: %d' }", id);
+            mockMvc.perform(get("/users/{id}", id))
+                    .andDo(print())
+                    .andExpect(status().isNotFound())
+                    .andExpect(content().json(expectedJson));
+        }
     }
 }
