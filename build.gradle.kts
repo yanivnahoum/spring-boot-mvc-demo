8e7197d98edbd68b736c4e674c3afa26c2b14d3e
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2021-03-20 22:14:44 +0200

    [build] Upgraded to Spring Boot 2.4.4, guava to 30.1.1-jre, spring-cloud-starter-contract-stub-runner to 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.4.3"
+    id("org.springframework.boot") version "2.4.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("io.freefair.lombok") version "5.3.0"
 }
@@ -21,12 +21,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.google.guava:guava:30.1-jre")
+    implementation("com.google.guava:guava:30.1.1-jre")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     runtimeOnly("mysql:mysql-connector-java")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner:3.0.0")
+    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner:3.0.2")
 
     implementation(platform("org.testcontainers:testcontainers-bom:1.15.2"))
     testImplementation("org.testcontainers:mysql")
