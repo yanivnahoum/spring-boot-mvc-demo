d067e6a758d8a9ea0752eb0d1a9bc281d00b0325
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2021-05-04 12:46:20 +0300

    [cleanup] Removed unused DTOs

--- src/test/java/com/att/training/spring/boot/demo/cache/AnotherPostCommentDto.java
+++
@@ -1,9 +0,0 @@
-package com.att.training.spring.boot.demo.cache;
-
-import lombok.Data;
-
-@Data
-public class AnotherPostCommentDto {
-    private String postTitle;
-    private String review;
-}
--- src/test/java/com/att/training/spring/boot/demo/cache/PostCommentDto.java
+++
@@ -1,9 +0,0 @@
-package com.att.training.spring.boot.demo.cache;
-
-import lombok.Value;
-
-@Value
-public class PostCommentDto {
-    String postTitle;
-    String review;
-}
