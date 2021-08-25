a740dc8087d8bcb6cd0d48a89c34b8dd21de7a9d
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2021-08-25 14:39:26 +0300

    [build] Bumped Spring Boot to 2.5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.5.3"
+    id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.freefair.lombok") version "5.3.3.3"
+    id("io.freefair.lombok") version "6.1.0"
 }
 
 group = "com.att.training.spring.boot"
@@ -40,10 +40,6 @@ tasks {
 
     jar {
         enabled = false
-    }
-
-    generateLombokConfig {
-        isEnabled = false
     }
 
     test {
