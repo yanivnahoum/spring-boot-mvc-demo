bfcc85aa1203a55870c49c7dacc818e9604ccd4a
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2021-09-26 13:46:37 +0300

    [build] Bumped Spring Boot to 2.5.5, lombok gradle plugin to 6.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.5.4"
+    id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.freefair.lombok") version "6.1.0"
+    id("io.freefair.lombok") version "6.2.0"
 }
 
 group = "com.att.training.spring.boot"
