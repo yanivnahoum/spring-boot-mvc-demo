a2ea15bdfe769a1097d367a1e855d5c4d571d53c
Nahoum, Yaniv (yn813h) <yn813h@intl.att.com>
2021-12-06 16:12:52 +0200

    Upgraded Spring Boot to 2.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.6.0"
+    id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("io.freefair.lombok") version "6.3.0"
 }
