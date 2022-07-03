21ed4fedf2e83c16a275fbc6f693994c9f39b4d6
Yaniv Nahoum <yn813h@intl.att.com>
2022-07-03 16:35:00 +0300

    Bumped spring boot to 2.7.1, io.spring.dependency-management to 1.0.12.RELEASE, io.freefair.lombok to 6.5.0.2, testcontainers to 1.17.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.7.0"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.freefair.lombok") version "6.4.3.1"
+    id("org.springframework.boot") version "2.7.1"
+    id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    id("io.freefair.lombok") version "6.5.0.2"
 }
 
 group = "com.att.training.spring.boot"
@@ -28,7 +28,7 @@ dependencies {
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.3"))
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
 
-    implementation(platform("org.testcontainers:testcontainers-bom:1.17.2"))
+    implementation(platform("org.testcontainers:testcontainers-bom:1.17.3"))
     testImplementation("org.testcontainers:mysql")
     testImplementation("org.testcontainers:junit-jupiter")
 }
