b3d1be711736b11f09583787b0e1d93e147bc74e
Yaniv Nahoum <yaniv.nahoum@intl.att.com>
2024-04-07 17:00:18 +0300

    Upgraded Spring Boot to 3.2.4, gradle to 8.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.1.5"
+    id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
-    id("io.freefair.lombok") version "8.4"
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "com.att.training.spring.boot"
@@ -18,36 +18,33 @@ configurations {
     }
 }
 
-extra["snakeyaml.version"] = "2.2"
-extra["testcontainers.version"] = "1.19.3"
 dependencies {
+    val guavaVersion = "33.1.0-jre"
+    val springCloudVersion = "2023.0.1"
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.google.guava:guava:32.1.2-jre")
+    implementation("com.google.guava:guava:$guavaVersion")
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion"))
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     runtimeOnly("com.mysql:mysql-connector-j")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
-    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2022.0.4"))
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
-
     testImplementation("org.testcontainers:mysql")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("com.squareup.okhttp3:mockwebserver")
     testImplementation("io.projectreactor:reactor-test")
-
-
 }
 
 tasks {
     withType<JavaCompile>().configureEach {
         with(options) {
-            release = 17
+            release = 21
             compilerArgs.add("-Xlint:all,-processing,-auxiliaryclass")
         }
     }
