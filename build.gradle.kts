7c48a815597ae2d54ccffddc7fab0d388837ffa0
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:27 +0300

    [polish] gradle boot, and dependency upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.gradle.api.JavaVersion.VERSION_11
+
 plugins {
     java
-    id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("org.springframework.boot") version "2.3.2.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("io.freefair.lombok") version "5.1.0"
 }
@@ -13,23 +15,23 @@ group = "com.att.training.spring.boot"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = VERSION_11
+    targetCompatibility = VERSION_11
 }
 
 tasks.generateLombokConfig {
     isEnabled = false
 }
 
-extra["rest-assured.version"] = "4.3.0"
-extra["groovy.version"] = "3.0.2" // upgrading from 2.x for rest-assured
+extra["rest-assured.version"] = "4.3.1"
+extra["groovy.version"] = "3.0.5" // upgrading from 2.x for rest-assured
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-    val swaggerVersion = "2.9.2"
+    val swaggerVersion = "3.0.0"
     implementation("io.springfox:springfox-swagger2:$swaggerVersion")
     implementation("io.springfox:springfox-swagger-ui:$swaggerVersion")
 
