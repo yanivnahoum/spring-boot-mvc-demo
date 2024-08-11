500a94d358cd0a39c8e7995fad781f4e4eb7ea07
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:32 +0300

    Upgraded Spring Boot to 3.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.2"
+    id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
-    id("io.freefair.lombok") version "8.4"
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "com.att.training.spring.boot"
