ef9880d82c5527423211c2eca69e5d11097ad6a5
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:27 +0300

    [CORS] Added CORS configuration (and commented out config in application.yml for management endpoints)

---
+++ src/main/java/com/att/training/spring/boot/demo/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.att.training.spring.boot.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
