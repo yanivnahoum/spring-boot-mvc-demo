615e1f13b5a993a09d23f84a6c78ce30b2ae8584
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:29 +0300

    [config] Used constant for http-service.url

--- src/test/java/com/att/training/spring/boot/demo/HttpServicePropertiesTest.java
+++ src/test/java/com/att/training/spring/boot/demo/HttpServicePropertiesTest.java
@@ -12,16 +12,19 @@ import org.springframework.test.context.TestPropertySource;
 
 import java.time.Duration;
 
+import static com.att.training.spring.boot.demo.HttpServicePropertiesTest.URL;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ContextConfiguration(classes = HttpServicePropertiesTest.TestConfiguration.class)
-@TestPropertySource(properties = "app.http-service.url=https://some-domain.com:8080")
+@TestPropertySource(properties = "app.http-service.url=" + URL)
 class HttpServicePropertiesTest {
 
     @EnableConfigurationProperties(HttpServiceProperties.class)
     @Configuration(proxyBeanMethods = false)
     static class TestConfiguration {}
+
+    static final String URL = "https://some-domain.com:8080";
 
     @TestPropertySource(properties = {
             "app.http-service.connect-timeout=5s",
@@ -36,7 +39,7 @@ class HttpServicePropertiesTest {
 
         @Test
         void immutablePojosCanBeUsedAsConfigurationProperties() {
-            var expectedProperties = new HttpServiceProperties("https://some-domain.com:8080", Duration.ofSeconds(5),
+            var expectedProperties = new HttpServiceProperties(URL, Duration.ofSeconds(5),
                     Duration.ofSeconds(10), 3);
             assertThat(httpServiceProperties).isEqualTo(expectedProperties);
         }
@@ -50,7 +53,7 @@ class HttpServicePropertiesTest {
 
         @Test
         void immutablePojosCanBeUsedAsConfigurationProperties() {
-            var expectedProperties = new HttpServiceProperties("https://some-domain.com:8080", Duration.ofSeconds(30),
+            var expectedProperties = new HttpServiceProperties(URL, Duration.ofSeconds(30),
                     Duration.ofSeconds(30), 5);
             assertThat(httpServiceProperties).isEqualTo(expectedProperties);
         }
