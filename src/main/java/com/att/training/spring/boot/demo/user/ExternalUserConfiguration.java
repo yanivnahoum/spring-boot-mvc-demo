aa777976217ffd0c66ceb1a29396113c794ed483
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:29 +0300

    [config] Used @ConfigurationPropertiesScan and removed @Component from @ConfigurationProperties classes, used new spring.config.import key for externalized yaml configuration

--- src/main/java/com/att/training/spring/boot/demo/user/ExternalUserConfiguration.java
+++ src/main/java/com/att/training/spring/boot/demo/user/ExternalUserConfiguration.java
@@ -1,11 +1,8 @@
 package com.att.training.spring.boot.demo.user;
 
-import com.att.training.spring.boot.demo.YamlPropertySourceFactory;
 import com.att.training.spring.boot.demo.api.User;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
@@ -13,8 +10,8 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-@PropertySource(factory = YamlPropertySourceFactory.class, value = "${app.config.dir:classpath:}external.yml")
+//@Component
+//@PropertySource(factory = YamlPropertySourceFactory.class, value = "${app.config.dir:classpath:}external.yml")
 @ConfigurationProperties(prefix = "ext")
 @Validated
 @Data
--- src/main/java/com/att/training/spring/boot/demo/user/UserConfiguration.java
+++ src/main/java/com/att/training/spring/boot/demo/user/UserConfiguration.java
@@ -3,14 +3,12 @@ package com.att.training.spring.boot.demo.user;
 import com.att.training.spring.boot.demo.api.User;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Component
 @ConfigurationProperties(prefix = "app")
 @Validated
 public @Data class UserConfiguration {
