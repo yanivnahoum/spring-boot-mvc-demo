4f486c53b197045a6646cf3aeb5ed46254e10254
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:28 +0300

    [performance] A PropertySourceFactory for yaml with better performance

--- src/main/java/com/att/training/spring/boot/demo/YamlPropertySourceFactory.java
+++ src/main/java/com/att/training/spring/boot/demo/YamlPropertySourceFactory.java
@@ -1,28 +1,30 @@
 package com.att.training.spring.boot.demo;
 
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.core.env.PropertiesPropertySource;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
 import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
 
-import java.util.Properties;
-
+@Slf4j
 public class YamlPropertySourceFactory implements PropertySourceFactory {
 
+    @SneakyThrows
     @NonNull
     @Override
-    public PropertySource<?> createPropertySource(String name, @NonNull EncodedResource resource) {
-        Properties yamlProperties = loadYamlProperties(resource);
-        String sourceName = getSourceName(name, resource);
-        return new PropertiesPropertySource(sourceName, yamlProperties);
-    }
-
-    private Properties loadYamlProperties(EncodedResource resource) {
-        var factory = new YamlPropertiesFactoryBean();
-        factory.setResources(resource.getResource());
-        return factory.getObject();
+    public PropertySource<?> createPropertySource(String name, @NonNull EncodedResource encodedResource) {
+        var yamlLoader = new YamlPropertySourceLoader();
+        var sourceName = getSourceName(name, encodedResource);
+        log.trace("#createPropertySource - Attempting to load {}", sourceName);
+        var propertySources = yamlLoader.load(sourceName, encodedResource.getResource());
+        Assert.notEmpty(propertySources, String.format("Yaml file [%s] must contain a single document", sourceName));
+        if (propertySources.size() > 1) {
+            log.warn("#createPropertySource - Yaml file [{}] loaded as property source contains multiple documents. Only the first one will be used.", sourceName);
+        }
+        return propertySources.get(0);
     }
 
     @NonNull
