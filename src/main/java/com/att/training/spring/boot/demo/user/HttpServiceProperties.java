622012a39b8ff22e7048883510749719fa837d2d
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:31 +0300

    Switched to records

--- src/main/java/com/att/training/spring/boot/demo/user/HttpServiceProperties.java
+++ src/main/java/com/att/training/spring/boot/demo/user/HttpServiceProperties.java
@@ -1,36 +1,18 @@
 package com.att.training.spring.boot.demo.user;
 
-import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotEmpty;
 import java.time.Duration;
 
-@ConstructorBinding
 @ConfigurationProperties(prefix = "app.http-service")
 @Validated
-@Value
-public class HttpServiceProperties {
-
-    @NotEmpty
-    String url;
-    Duration connectTimeout;
-    Duration readTimeout;
-    int maxRetries;
-
-    public HttpServiceProperties(
-            String url,
-            @DefaultValue("30s") Duration connectTimeout,
-            @DefaultValue("30s") Duration readTimeout,
-            @DefaultValue("5") int maxRetries
-    ) {
-        this.url = url;
-        this.connectTimeout = connectTimeout;
-        this.readTimeout = readTimeout;
-        this.maxRetries = maxRetries;
-    }
-}
+public record HttpServiceProperties (
+    @NotEmpty String url,
+    @DefaultValue("30s") Duration connectTimeout,
+    @DefaultValue("30s") Duration readTimeout,
+    @DefaultValue("5") int maxRetries
+) { }
 
