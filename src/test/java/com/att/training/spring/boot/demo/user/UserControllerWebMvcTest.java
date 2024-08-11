a930a6f31cdb4436c607a3e7919cbdee078954cf
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:26 +0300

    [demo] Upgraded to 2.1.3.RELEASE, removed SpringExtension from @SpringBootTest and @WebMvcTest (already included), added CRLF injection mitigation pattern in logback.xml

--- src/test/java/com/att/training/spring/boot/demo/user/UserControllerWebMvcTest.java
+++ src/test/java/com/att/training/spring/boot/demo/user/UserControllerWebMvcTest.java
@@ -4,11 +4,9 @@ import com.att.training.spring.boot.demo.api.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -20,7 +18,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(UserController.class)
 class UserControllerWebMvcTest {
 
