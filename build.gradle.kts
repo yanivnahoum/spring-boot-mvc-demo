0ef3b5afba59e7786c928a701d21a94ab2bb2723
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-11 20:30:31 +0300

    Added annotation processor

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,15 @@ repositories {
     mavenCentral()
 }
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
