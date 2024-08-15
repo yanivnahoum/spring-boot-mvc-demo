6227d27e07afb5f505ad472d8dd180a03356d053
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-15 11:22:54 +0300

    Stored prototype index privately

--- src/main/java/com/att/training/spring/boot/demo/scopes/MyPrototype.java
+++ src/main/java/com/att/training/spring/boot/demo/scopes/MyPrototype.java
@@ -1,5 +1,6 @@
 package com.att.training.spring.boot.demo.scopes;
 
+import lombok.Getter;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -7,17 +8,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
+@Getter
 @Component
 @Scope(SCOPE_PROTOTYPE)
 class MyPrototype {
-
     private static final AtomicInteger counter = new AtomicInteger();
+    private final int ordinal;
 
     public MyPrototype() {
-        counter.incrementAndGet();
-    }
-
-    public int getOrdinal() {
-        return counter.get();
+        ordinal = counter.incrementAndGet();
     }
 }
