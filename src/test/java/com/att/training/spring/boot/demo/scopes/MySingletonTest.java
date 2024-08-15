1b243acc4a89148c566ed6c242f35d3e77cf1239
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-08-15 11:30:28 +0300

    Added order in MySingletonTest

--- src/test/java/com/att/training/spring/boot/demo/scopes/MySingletonTest.java
+++ src/test/java/com/att/training/spring/boot/demo/scopes/MySingletonTest.java
@@ -1,6 +1,9 @@
 package com.att.training.spring.boot.demo.scopes;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -10,22 +13,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {MySingleton.class, MyPrototype.class})
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MySingletonTest {
 
     @Autowired
     private MySingleton mySingleton;
 
-    @Test
-    void prototypeFields_shouldBeUniqueInstances() {
-        assertThat(mySingleton.getMyPrototype1()).isNotSameAs(mySingleton.getMyPrototype2());
-    }
-
+    @Order(1)
     @Test
     void getPrototypeOrdinal() {
         int startOrdinal = mySingleton.getPrototypeOrdinal();
         for (int i = 1; i <= 10; i++) {
             assertThat(mySingleton.getPrototypeOrdinal()).isEqualTo(startOrdinal + i);
         }
+    }
+
+    @Order(2)
+    @Test
+    void prototypeFields_shouldBeUniqueInstances() {
+        assertThat(mySingleton.getMyPrototype1()).isNotSameAs(mySingleton.getMyPrototype2());
     }
 }
 
