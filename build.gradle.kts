f5781a2e97c092640fc02f2214ffadd576ba2bfb
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-10-25 09:40:44 +0300

    Upgraded Spring Boot, dependencies, and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.4"
+    id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
-    id("io.freefair.lombok") version "8.10"
+    id("io.freefair.lombok") version "8.10.2"
 }
 
 group = "com.att.training.spring.boot"
@@ -19,7 +19,7 @@ configurations {
 }
 
 dependencies {
-    val guavaVersion = "33.3.0-jre"
+    val guavaVersion = "33.3.1-jre"
     val springdocVersion = "2.6.0"
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
