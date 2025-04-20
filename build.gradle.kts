8bbb98679fd0144a82af021e57613a99388120de
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-04-20 11:57:59 +0300

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.3"
+    id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("io.freefair.lombok") version "8.13"
 }
@@ -25,7 +25,7 @@ configurations {
 }
 
 dependencies {
-    val guavaVersion = "33.4.5-jre"
+    val guavaVersion = "33.4.8-jre"
     val springdocVersion = "2.8.6"
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
