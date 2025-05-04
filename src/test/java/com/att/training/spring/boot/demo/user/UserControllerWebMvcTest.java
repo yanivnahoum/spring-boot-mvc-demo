5973f94dc9586403972164f486de03ed9df0f5ff
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-05-04 17:21:12 +0300

    Refactored to use MockMvcTester

--- src/test/java/com/att/training/spring/boot/demo/user/UserControllerWebMvcTest.java
+++ src/test/java/com/att/training/spring/boot/demo/user/UserControllerWebMvcTest.java
@@ -7,23 +7,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 import java.util.List;
 
-import static com.att.training.spring.boot.demo.utils.JsonUtils.singleToDoubleQuotes;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerWebMvcTest {
-
     private static final User JOHN_DOE = new User(17, "John", "Doe", 30);
     private static final List<User> USERS = List.of(JOHN_DOE, JOHN_DOE, JOHN_DOE);
     private static final List<User> SINGLE_USER = List.of(JOHN_DOE);
@@ -31,42 +25,61 @@ class UserControllerWebMvcTest {
     @MockitoBean
     private UserService userService;
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvcTester mockMvc;
 
     @Nested
     @DisplayName("When calling GET /users")
     class GetAllUsers {
-
         @Test
-        void givenServiceReturnsMultipleUsers_shouldReturn200OK_withMultipleUsers() throws Exception {
+        void givenServiceReturnsMultipleUsers_shouldReturn200OK_withMultipleUsers() {
             when(userService.fetchAll()).thenReturn(USERS);
-            mockMvc.perform(get("/users"))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.length()", is(equalTo(USERS.size()))));
+
+            var result = mockMvc.get().uri("/users");
+
+            assertThat(result)
+                    .hasStatusOk()
+                    .bodyJson()
+                    .extractingPath("$.length()").isEqualTo(USERS.size());
+
         }
 
         @Test
-        void givenServiceReturnsSingleUser_shouldReturn200OK_withSingleUser() throws Exception {
+        void givenServiceReturnsSingleUser_shouldReturn200OK_withSingleUser() {
             when(userService.fetchAll()).thenReturn(SINGLE_USER);
-            mockMvc.perform(get("/users"))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.length()", is(equalTo(SINGLE_USER.size()))));
+
+            var result = mockMvc.get().uri("/users");
+
+            assertThat(result)
+                    .hasStatusOk()
+                    .bodyJson()
+                    .extractingPath("$.length()").isEqualTo(SINGLE_USER.size());
         }
 
         @Test
-        void givenServiceReturnsEmptyList_shouldReturn200OK_withNoUsers() throws Exception {
-            mockMvc.perform(get("/users"))
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.length()", is(equalTo(0))));
-        }
+        void givenServiceReturnsEmptyList_shouldReturn200OK_withNoUsers() {
+            var result = mockMvc.get().uri("/users");
 
+            assertThat(result)
+                    .hasStatusOk()
+                    .bodyJson()
+                    .extractingPath("$.length()").isEqualTo(0);
+        }
     }
 
     @Test
-    void givenUserInBody_whenPUT_shouldReturn200OK() throws Exception {
-        mockMvc.perform(put("/users")
+    void givenUserInBody_whenPUT_shouldReturn200OK() {
+        var result = mockMvc.put()
+                .uri("/users")
                 .contentType(APPLICATION_JSON)
-                .content(singleToDoubleQuotes("{'id':1,'firstName':'Michael','lastName':'Jordan','age':50}")))
-                .andExpect(status().isNoContent());
+                .content("""
+                        {
+                            "id": 1,
+                            "firstName": "Michael",
+                            "lastName": "Jordan",
+                            "age": 50
+                        }
+                        """);
+
+        assertThat(result).hasStatus(NO_CONTENT);
     }
 }
