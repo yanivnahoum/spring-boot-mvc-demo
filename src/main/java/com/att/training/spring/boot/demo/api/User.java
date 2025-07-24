d34b252a979ac6bea2905e4ba7145916e1784d4d
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-07-24 10:53:33 +0300

    Used @Positive on long ind field

--- src/main/java/com/att/training/spring/boot/demo/api/User.java
+++ src/main/java/com/att/training/spring/boot/demo/api/User.java
@@ -1,11 +1,11 @@
 package com.att.training.spring.boot.demo.api;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.Range;
 
 public record User(
-        @Min(1) long id,
+        @Positive long id,
         @NotEmpty String firstName,
         @NotEmpty String lastName,
         @Range(min = 18, max = 120) int age
