dc4b2213751847c7ea7c99fa3275ce2effbbdb9f
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-10-23 20:00:53 +0300

    Refactor JacksonDefaultValueTest to use nested classes for better organization and add tests for null field handling in records with and without defaults

--- src/test/java/com/att/training/spring/boot/demo/serdes/JacksonDefaultValueTest.java
+++ src/test/java/com/att/training/spring/boot/demo/serdes/JacksonDefaultValueTest.java
@@ -2,6 +2,9 @@ package com.att.training.spring.boot.demo.serdes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.extern.jackson.Jacksonized;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -10,36 +13,74 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 class JacksonDefaultValueTest {
+    private static final String EMPTY_JSON = "{}";
+    private static final String NULL_VALUE_JSON = """
+            { "value": null }
+            """;
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    void givenRecordWithNoDefault_thenMissingFieldIsNull() throws JsonProcessingException {
-        var somePojo = objectMapper.readValue("{}", SomePojo.class);
-        assertThat(somePojo.value()).isNull();
+    @Nested
+    class RecordWithNoDefault {
+        @Test
+        void givenRecordWithNoDefault_thenMissingFieldIsNull() throws JsonProcessingException {
+            var somePojo = objectMapper.readValue(EMPTY_JSON, SomePojo.class);
+            assertThat(somePojo.value()).isNull();
+        }
+
+        @Test
+        void givenRecordWithNoDefault_thenNullFieldIsNull() throws JsonProcessingException {
+            var somePojo = objectMapper.readValue(NULL_VALUE_JSON, SomePojo.class);
+            assertThat(somePojo.value()).isNull();
+        }
+
+        record SomePojo(String value) {}
     }
 
-    @Test
-    void givenRecordWithDefault_thenMissingFieldEqualsDefault() throws JsonProcessingException {
-        var somePojoWithDefault = objectMapper.readValue("{}", SomePojoWithDefault.class);
-        assertThat(somePojoWithDefault.value()).isEqualTo("default");
+    @Nested
+    class RecordWithDefault {
+        @Test
+        void givenRecordWithDefault_thenMissingFieldEqualsDefault() throws JsonProcessingException {
+            var somePojoWithDefault = objectMapper.readValue("{}", SomePojoWithDefault.class);
+            assertThat(somePojoWithDefault.value()).isEqualTo("default");
+        }
+
+
+        @Test
+        void givenRecordWithDefault_thenNullFieldEqualsDefault() throws JsonProcessingException {
+            var somePojoWithDefault = objectMapper.readValue(NULL_VALUE_JSON, SomePojoWithDefault.class);
+            assertThat(somePojoWithDefault.value()).isEqualTo("default");
+        }
+
+        record SomePojoWithDefault(String value) {
+            SomePojoWithDefault {
+                if (value == null) {
+                    value = "default";
+                }
+            }
+        }
     }
 
-    @Test
-    void givenRecordWithDefault_thenNullFieldEqualsDefault() throws JsonProcessingException {
-        var json = """
-                { "value": null}
-                """;
-        var somePojoWithDefault = objectMapper.readValue(json, SomePojoWithDefault.class);
-        assertThat(somePojoWithDefault.value()).isEqualTo("default");
-    }
+    @Nested
+    class RecordWithLombokBuilder {
+        @Test
+        void givenBuilderRecordWithDefault_thenMissingFieldEqualsDefault() throws JsonProcessingException {
+            var someLombokBuilderPojo = objectMapper.readValue(EMPTY_JSON, SomeLombokBuilderPojo.class);
+            assertThat(someLombokBuilderPojo.value()).isEqualTo("default");
+        }
 
-    record SomePojo(String value) {}
+        @Test
+        void givenBuilderRecordWithDefault_thenNullFieldEqualsNull() throws JsonProcessingException {
+            var someLombokBuilderPojo = objectMapper.readValue(NULL_VALUE_JSON, SomeLombokBuilderPojo.class);
+            assertThat(someLombokBuilderPojo.value()).isNull();
+        }
 
-    record SomePojoWithDefault(String value) {
-        SomePojoWithDefault {
-            if (value == null) {
-                value = "default";
+        @Jacksonized
+        @Builder
+        record SomeLombokBuilderPojo(String value) {
+            @SuppressWarnings({"unused", "FieldMayBeFinal"})
+            public static class SomeLombokBuilderPojoBuilder {
+                private String value = "default";
             }
         }
     }
