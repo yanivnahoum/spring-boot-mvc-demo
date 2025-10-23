e8588c494c6c86ff30ce6cd33a68a440f735a8c2
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2025-10-23 18:37:27 +0300

    Add JacksonDefaultValueTest to verify default value behavior in JSON deserialization

---
+++ src/test/java/com/att/training/spring/boot/demo/serdes/JacksonDefaultValueTest.java
@@ -0,0 +1,37 @@
+package com.att.training.spring.boot.demo.serdes;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class JacksonDefaultValueTest {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void givenRecordWithNoDefault_thenMissingFieldIsNull() throws JsonProcessingException {
+        var somePojo = objectMapper.readValue("{}", SomePojo.class);
+        assertThat(somePojo.value()).isNull();
+    }
+
+    @Test
+    void givenRecordWithDefault_thenMissingFieldEqualsDefault() throws JsonProcessingException {
+        var somePojoWithDefault = objectMapper.readValue("{}", SomePojoWithDefault.class);
+        assertThat(somePojoWithDefault.value()).isEqualTo("default");
+    }
+
+    record SomePojo(String value) {}
+
+    record SomePojoWithDefault(String value) {
+        SomePojoWithDefault {
+            if (value == null) {
+                value = "default";
+            }
+        }
+    }
+}
