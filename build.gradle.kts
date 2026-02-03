aaabe105cb8b3521a9cb1de0186a1ccded727410
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2026-02-03 12:52:00 +0200

    Update dependencies to latest versions: Maven 3.9.12, Gradle 9.3.1, Spring Boot 3.5.10, and Java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.9"
+    id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
     id("io.freefair.lombok") version "9.2.0"
 }
@@ -10,7 +10,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
