a36b575172763cc30668725b9bc9d840b5a4719d
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2026-04-24 19:58:27 +0300

    Bump Spring Boot version to 4.0.6 in build.gradle.kts and pom.xml, and remove unused test configuration in JacksonBigDecimalTest.java after Jackson 3.1.1 bug fix: https://github.com/FasterXML/jackson-databind/issues/5819

--- src/test/java/com/att/training/spring/boot/demo/serdes/JacksonBigDecimalTest.java
+++ src/test/java/com/att/training/spring/boot/demo/serdes/JacksonBigDecimalTest.java
@@ -2,14 +2,10 @@ package com.att.training.spring.boot.demo.serdes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.json.BasicJsonTester;
-import org.springframework.context.annotation.Bean;
 import tools.jackson.core.JacksonException;
 import tools.jackson.databind.JsonNode;
-import tools.jackson.databind.cfg.JsonNodeFeature;
 import tools.jackson.databind.json.JsonMapper;
 
 import java.math.BigDecimal;
@@ -19,27 +15,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 class JacksonBigDecimalTest {
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        JsonMapperBuilderCustomizer customizer() {
-            return builder -> builder
-//                    .enable(StreamWriteFeature.WRITE_BIGDECIMAL_AS_PLAIN)
-                    .enable(JsonNodeFeature.STRIP_TRAILING_BIGDECIMAL_ZEROES);
-//                            .configure(JsonNodeFeature.STRIP_TRAILING_BIGDECIMAL_ZEROES, false)
-//                            .configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true)
-//                                    .configOverride(BigDecimal.class).setFormat(JsonFormat.Value.forShape(STRING))
-        }
-    }
-
     private final BasicJsonTester json = new BasicJsonTester(getClass());
     @Autowired
     private JsonMapper mapper;
 
     @Test
     void givenBigDecimalWithTrailingZero_whenReadValue_thenTrailingZeroIsKept() throws JacksonException {
-        String json = singleToDoubleQuotes("{ 'value': 2.00 }");
-        var bigDecimalHolder = mapper.readValue(json, BigDecimalHolder.class);
+        String jsonString = singleToDoubleQuotes("{ 'value': 2.00 }");
+        var bigDecimalHolder = mapper.readValue(jsonString, BigDecimalHolder.class);
         assertThat(bigDecimalHolder.value().toPlainString()).isEqualTo("2.00");
     }
 
