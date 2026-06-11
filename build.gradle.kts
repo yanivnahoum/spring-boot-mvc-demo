da970315e8bc59f86a65fe202bf9310c65fdc919
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2026-06-11 13:43:27 +0300

    Bump Spring Boot version to 4.0.7 and update Lombok to 9.5.0; upgrade Guava to 33.6.0-jre and Springdoc to 3.0.3 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.6"
+    id("org.springframework.boot") version "4.0.7"
     id("io.spring.dependency-management") version "1.1.7"
-    id("io.freefair.lombok") version "9.4.0"
+    id("io.freefair.lombok") version "9.5.0"
 }
 
 group = "com.att.training.spring.boot"
@@ -26,8 +26,8 @@ configurations {
 
 val mockitoAgent = configurations.create("mockitoAgent")
 dependencies {
-    val guavaVersion = "33.5.0-jre"
-    val springdocVersion = "3.0.2"
+    val guavaVersion = "33.6.0-jre"
+    val springdocVersion = "3.0.3"
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-aspectj")
