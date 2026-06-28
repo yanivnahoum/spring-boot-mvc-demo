1083013fb903118cebac257762ac367249c6ba79
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2026-06-28 13:47:52 +0300

    Update Spring Boot to 3.5.16 and Gradle to 9.6.1;

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.15"
+    id("org.springframework.boot") version "3.5.16"
     id("io.spring.dependency-management") version "1.1.7"
     id("io.freefair.lombok") version "9.5.0"
 }
